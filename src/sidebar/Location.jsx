8095d357da962e4a43222fa0fbf345713981b77e
martinondiwa <surekondiwa@gmail.com>
2024-10-13 21:04:52 +0300

    ad

--- src/sidebar/Location.jsx
+++ src/sidebar/Location.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Location = ({handleChange, handleClick}) => {
     return (
       <div className='space-y-5'>
-      <h3 className='text-lg font-bold mb-2'>Location</h3>
+      <h4 className='text-lg font-bold mb-2'>Location</h4>
       </div>
     )
   }
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -6,7 +6,7 @@ const Sidebar = ({handleChange, handleClick}) => {
     <div className='space-y-5'>
     <h3 className='text-lg font-bold mb-2'>Filters</h3>
 
-    <Location/>
+    <Location handleChange={handleChange}/>
     </div>
   )
 }
