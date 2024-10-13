dec514f52eb2a19a72e625d7d1cd2e0e0c050ff4
martinondiwa <surekondiwa@gmail.com>
2024-10-13 14:22:42 +0300

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,9 +41,17 @@ const Navbar = () => {
           ))}
         </ul>
         {/**Sign up & Login Buttons */}
-        <div className="text-base text-primary font-medium space-x-5 hidden lg:block"> </div>
-        <Link to="/login" className="py-2 px-5 border rounded">Log in </Link>
-        <Link to="/sign-up" className="py-2 px-5 border rounded bg-blue text-white">Sign Up</Link>
+        <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
+          <Link to="/login" className="py-2 px-5 border rounded">
+            Log in{" "}
+          </Link>
+          <Link
+            to="/sign-up"
+            className="py-2 px-5 border rounded bg-blue text-white"
+          >
+            Sign Up
+          </Link>
+        </div>
       </nav>
     </header>
   );
