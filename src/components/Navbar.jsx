2674017a657208b98c9054bc7d178c9b1a668c0e
martinondiwa <surekondiwa@gmail.com>
2024-10-13 13:51:10 +0300

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { SiOpensearch } from "react-icons/si";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-    const [isMenuOpen,setIsMenuOpen]= useState(false);
-    const handleMenuToggler = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuToggler = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    const navItems = [
-        {path:"/", title:"Start a Search"},
-        {path:"/my-job", title:"My Jobs"},
-        {path:"/salary", title:"Salary Estimate"},
-        {path:"/post-job", title:"Post A Job"},
-    ]
+  const navItems = [
+    { path: "/", title: "Start a Search" },
+    { path: "/my-job", title: "My Jobs" },
+    { path: "/salary", title: "Salary Estimate" },
+    { path: "/post-job", title: "Post A Job" },
+  ];
   return (
- <header>
-    <nav>
-    <a href="/" className='flex items-center gap-2 text-2xl text-black'> <SiOpensearch />
-    <span>Quick Speed Job Search</span></a>
-    {/** Nav items for large devices */}
-    <ul>
-     {
-        navItems.map(({path, title}) => (
+    <header>
+      <nav>
+        <a href="/" className="flex items-center gap-2 text-2xl text-black">
+          {" "}
+          <SiOpensearch />
+          <span>Quick Speed Job Search</span>
+        </a>
+        {/** Nav items for large devices */}
+        <ul>
+          {navItems.map(({ path, title }) => (
             <li key={path}>
-            <NavLink></NavLink>
+              <NavLink
+                to="/messages"
+                className={({ isActive, isPending, isTransitioning }) =>
+                  [
+                    isPending ? "pending" : "",
+                    isActive ? "active" : "",
+                    isTransitioning ? "transitioning" : "",
+                  ].join(" ")
+                }
+              >
+                Messages
+              </NavLink>
             </li>
-        ))
-     }
-    </ul>
-    </nav>
- </header>
-  )
-}
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
