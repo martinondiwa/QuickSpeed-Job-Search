072aa3f0a5b87cc7a4d46d4032ac42d6530933ab
martinondiwa <surekondiwa@gmail.com>
2024-10-13 16:51:14 +0300

    asd

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,6 +5,7 @@ const Banner = () => {
     const [query, setQuery] = useState ("");
     const handleInputChange = (event) => {
         setQuery(event.target.value)
+        console.log(event.target.value)
     }
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-4 px-8 md:py-20 py-14">
