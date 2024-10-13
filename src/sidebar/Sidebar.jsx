353c4f533a1d8b855c3a25c11adb47cce07121ce
martinondiwa <surekondiwa@gmail.com>
2024-10-13 20:59:41 +0300

    asd

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Sidebar = ({handleChange, handleClick}) => {
   return (
-    <div>
-      
+    <div className='space-y-5'>
+    <h3 className='text-lg font-bold mb-2'>Filters</h3>
     </div>
   )
 }
