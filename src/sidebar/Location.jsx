233cdff27eab4b7c10ab4444cb65763905a746d4
martinondiwa <surekondiwa@gmail.com>
2024-10-13 21:35:17 +0300

    asd

--- src/sidebar/Location.jsx
+++ src/sidebar/Location.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import InputField from '../components/InputField'
+import React from 'react';
+import InputField from '../components/InputField';
 
-const Location = ({handleChange}) => {
+const Location = ({ handleChange }) => {
     return (
-      <div >
-      <h4 className='text-lg font-medium mb-2'>Location</h4>
-       <div>
-       <label htmlFor="" className='sidebar-label-container'>
-          <input type="radio" name='test' id='test' value="" onChange={handleChange}/>
-          <span className='checkmark'></span> All
-       </label>
+        <div>
+            <h4 className='text-lg font-medium mb-2'>Location</h4>
+            <div>
+                <label className='sidebar-label-container'>
+                    <input
+                        type="radio"
+                        name='location'
+                        value=""
+                        onChange={handleChange}
+                    />
+                    <span className='checkmark'></span> All
+                </label>
 
-       <InputField handleChange={handleChange} value="london" title="London" name="test"/>
-       
-       <InputField handleChange={handleChange} value="seattle" title="Seattle" name="test"/>
+                <InputField handleChange={handleChange} value="london" title="London" name="location" />
+                <InputField handleChange={handleChange} value="seattle" title="Seattle" name="location" />
+                <InputField handleChange={handleChange} value="brussels" title="Brussels" name="location" />
+                <InputField handleChange={handleChange} value="san Francisco" title="San Francisco" name="location" />
+                <InputField handleChange={handleChange} value="boston" title="Boston" name="location" />
+                <InputField handleChange={handleChange} value="madrid" title="Madrid" name="location" />
+            </div>
+        </div>
+    );
+};
 
-       
-       <InputField handleChange={handleChange} value="brussels" title="Brussels" name="test"/>
-
-       
-       <InputField handleChange={handleChange} value="san Francisco" title=" San Francisco" name="test"/>
-
-       
-       <InputField handleChange={handleChange} value="boston" title="Boston" name="test"/>
-
-       
-       <InputField handleChange={handleChange} value="madrid" title="Madrid" name="test"/>
-       </div>
-      </div>
-    )
-  }
-export default Location
+export default Location;
