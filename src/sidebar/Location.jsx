fac6c5bf4011d652147259c2d7b984fc45aef301
martinondiwa <surekondiwa@gmail.com>
2024-10-13 21:02:56 +0300

    ad

---
+++ src/sidebar/Location.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Location = ({handleChange, handleClick}) => {
+    return (
+      <div className='space-y-5'>
+      <h3 className='text-lg font-bold mb-2'>Location</h3>
+      </div>
+    )
+  }
+export default Location
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Location from './Location'
 
 const Sidebar = ({handleChange, handleClick}) => {
   return (
     <div className='space-y-5'>
     <h3 className='text-lg font-bold mb-2'>Filters</h3>
+
+    <Location/>
     </div>
   )
 }
