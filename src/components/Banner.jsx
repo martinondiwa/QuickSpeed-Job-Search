846a51c7e1fdae8025dce8df2ed8f0091f9cc377
martinondiwa <surekondiwa@gmail.com>
2024-10-13 16:08:19 +0300

    asd

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className='max-w-screen-2xl container mx-auto xl:px-4 md:py-20 py-14'>
-      <h1 className='text-5xl font-bold text-primary mb-3'>Find your <span className='text-blue'>new job </span>today</h1>
+      <h1 className='text-5xl font-bold text-primary mb-3'>Find your <span className='text-blue'>Perfect Job</span> Match today</h1>
+      <p>Discover your next career opportunity with ease – search, apply, and land your dream job today!</p>
     </div>
   )
 }
