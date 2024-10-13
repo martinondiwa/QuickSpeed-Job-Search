af3e725aefd512ea87445cc184e01abed80b2f3d
martinondiwa <surekondiwa@gmail.com>
2024-10-13 17:38:49 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
   useEffect (() => {
     fetch ("jobs.json").then(res => res.json()).then(data =>{
       console.log(data)
+      setJobs(data)
     })
   }, [])
-
+    console.log(jobs)
   const [query, setQuery] = useState ("");
   const handleInputChange = (event) => {
       setQuery(event.target.value)
