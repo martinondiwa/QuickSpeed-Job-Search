a0a0ea2f9d24bef1fc1ed8f627616fb1055ffbb3
martinondiwa <surekondiwa@gmail.com>
2024-10-13 18:56:52 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,71 +1,86 @@
 import { useEffect, useState } from "react";
-import Banner from "../components/Banner"
+import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Jobs from "./Jobs";
 
 const Home = () => {
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const[jobs, setJobs] = useState ([]);
+  const [jobs, setJobs] = useState([]);
 
-  useEffect (() => {
-    fetch ("jobs.json").then(res => res.json()).then(data =>{
-      //console.log(data)
-      setJobs(data)
-    })
-  }, [])
+  useEffect(() => {
+    fetch("jobs.json")
+      .then((res) => res.json())
+      .then((data) => {
+        //console.log(data)
+        setJobs(data);
+      });
+  }, []);
 
-    //console.log(jobs)
- 
+  //console.log(jobs)
+
   //handle input change
-  const [query, setQuery] = useState ("");
+  const [query, setQuery] = useState("");
   const handleInputChange = (event) => {
-      setQuery(event.target.value)
-  }
- //filter jobs by title
- const filterItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+    setQuery(event.target.value);
+  };
+  //filter jobs by title
+  const filterItems = jobs.filter(
+    (job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
 
- //..........Radio Filtering......
- const handleChange= (event) => {
-  setSelectedCategory(event.target.value)
- }
- //.....button based filtering....
- const handleClick= (event) => {
-  setSelectedCategory(event.target.value)
- }
- //main function
- const filteredData = (jobs, selected, query) => {
-  let filteredJobs = jobs;
+  //..........Radio Filtering......
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  //.....button based filtering....
+  const handleClick = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  //main function
+  const filteredData = (jobs, selected, query) => {
+    let filteredJobs = jobs;
 
-  //filtering input items
-  if(query) {
-    filteredJobs=filterItems;
-  }
+    //filtering input items
+    if (query) {
+      filteredJobs = filterItems;
+    }
 
-  //category filtering 
-  if(selected){
-    filteredJobs=filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) =>(
-      jobLocation.toLowerCase()===selected.toLowerCase() ||
-      parseInt(maxPrice) <= parseInt(selected)||
-      salaryType.toLowerCase()===selected.toLowerCase() ||
-      employmentType.toLowerCase() === selected.toLowerCase()
-    ));
-    console.log(filteredJobs)
-  }
-  return filteredJobs.map((data, i)=> <Card key={i} data={data}/>)
- }
+    //category filtering
+    if (selected) {
+      filteredJobs = filteredJobs.filter(
+        ({
+          jobLocation,
+          maxPrice,
+          experienceLevel,
+          salaryType,
+          employmentType,
+          postingDate,
+        }) =>
+          jobLocation.toLowerCase() === selected.toLowerCase() ||
+          parseInt(maxPrice) <= parseInt(selected) ||
+          salaryType.toLowerCase() === selected.toLowerCase() ||
+          employmentType.toLowerCase() === selected.toLowerCase()
+      );
+      console.log(filteredJobs);
+    }
+    return filteredJobs.map((data, i) => <Card key={i} data={data} />);
+  };
 
-  const result =filteredData(jobs, selectedCategory, query);
+  const result = filteredData(jobs, selectedCategory, query);
 
   return (
     <div className="text-blue">
-      <Banner query={query} handleInputChange={handleInputChange}/>
-      
-      <div></div>
-      <div><Jobs result={result}/></div>
-      <div></div>
-    </div>
-    
-  )
-}
+      <Banner query={query} handleInputChange={handleInputChange} />
 
-export default Home
+      <div>
+        <div>Left</div>
+        <div>
+          <Jobs result={result} />
+        </div>
+        <div>Right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
