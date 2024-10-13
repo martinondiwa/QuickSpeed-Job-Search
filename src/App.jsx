0b242b9725a99b9cf76bbfe129accb5eec2a4cb5
martinondiwa <surekondiwa@gmail.com>
2024-10-13 12:14:09 +0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   
   return (
     <>
-      <h1 className='text-5xl font-bold text-indigo-700'> Hello Developer</h1>
+   <Outlet/>
     </>
   )
 }
