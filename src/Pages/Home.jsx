b0bd7141628d76aef67e02ed094e408bd2069982
martinondiwa <surekondiwa@gmail.com>
2024-10-13 17:46:10 +0300

    ad

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -19,6 +19,7 @@ const Home = () => {
       setQuery(event.target.value)
   }
  //filter jobs by title
+ const filterItems = jobs.filter((jobs) => job.jobTitle )
   return (
     <div className="text-blue">
       <Banner query={query} handleInputChange={handleInputChange}/>
