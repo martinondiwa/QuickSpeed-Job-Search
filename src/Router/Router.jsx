e20db3a6f95f6a1c5a0df8eda31eb1caad4302e7
martinondiwa <surekondiwa@gmail.com>
2024-10-13 12:12:25 +0300

    AS

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
     {
@@ -7,7 +8,7 @@ const router = createBrowserRouter([
       element: <App/>,
       children: [
         {path: "/",
-        element: 
+        element: <Home/>
         }
       ]
     },
