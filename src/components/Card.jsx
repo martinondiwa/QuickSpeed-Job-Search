92cebb4639bb76ce8df4598c2cb5a4f04a9cb27a
martinondiwa <surekondiwa@gmail.com>
2024-10-13 19:39:11 +0300

    as

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Card = ({data}) => {
+const Card = ({ data }) => {
+    const {
+        companyName,
+        companyLogo,
+        minPrice,
+        maxPrice,
+        salaryType,
+        jobLocation,
+        employmentType,
+        postingDate,
+        description
+    } = data;
 
-    const Card = ({data})=> { 
-        const {companyName, companyLogo, minPrice, maxPrice, salaryType, jobLocation, employmentType,postingDate, description} = data; 
-    
-  return (
-   <section className='card'>
-   <Link to={"/"} className='flex gap-4 flex-col sm:flex-row items-start'>
-    <img src={companyLogo} alt="" />
-   </Link>
-   </section>
-  );
-}
+    return (
+        <section className='card'>
+            <Link to="/" className='flex gap-4 flex-col sm:flex-row items-start'>
+                <img src={companyLogo} alt={companyName} />
+                <div>
+                    <h3>{companyName}</h3>
+                    <p>{jobLocation}</p>
+                    <p>{employmentType}</p>
+                    <p>{minPrice} - {maxPrice} ({salaryType})</p>
+                    <p>{postingDate}</p>
+                    <p>{description}</p>
+                </div>
+            </Link>
+        </section>
+    );
 }
 
 export default Card;
