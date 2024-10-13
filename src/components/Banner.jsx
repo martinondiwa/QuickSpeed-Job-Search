b614cad941c65c1015e914abd6f5ff8fbe575791
martinondiwa <surekondiwa@gmail.com>
2024-10-13 16:46:06 +0300

    ad

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { IoSearchCircleSharp } from "react-icons/io5";
 
 const Banner = () => {
     const [query, setQuery] = useState ("");
@@ -21,6 +22,7 @@ const Banner = () => {
            <input type="text" name="title" id="title" placeholder="What position are you looking for?"  className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholdercolor: text-gray-400 focus:right-0 sm:leading-6
            " onChange={handleInputChange}
            value={query}/>
+           <IoSearchCircleSharp />
           </div>
         </div>
       </form>
