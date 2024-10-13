96f4a7762ac3932649bad82108f85c00e59e8630
martinondiwa <surekondiwa@gmail.com>
2024-10-13 16:02:13 +0300

    asd

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className='max-w-screen-2xl container mx-auto xl:px-4 md:py-20 py-14'>
-      <h1 className='text-5xl font-bold text-primary mb-3'>Find your <span className='text-'>New Job Today</span></h1>
+      <h1 className='text-5xl font-bold text-primary mb-3'>Find your <span className='text-blue'>New Job Today</span></h1>
     </div>
   )
 }
