d07cfb19c2d8cf4181f1cf6cd5a17b1842092829
martinondiwa <surekondiwa@gmail.com>
2024-10-13 22:50:16 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -58,7 +58,9 @@ const Home = () => {
             <div className="bg-slate-300 md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12">
                 <div className="bg-white p-4 rounded"><Sidebar handleChange={handleChange} /></div>
                 <div className="col-span-2 bg-slate-200 p-4 rounded-md">
-                    <Jobs result={result} />
+                  {
+                    isLoading ? (<P>Loading.....</P>) :   <Jobs result={result} />
+                  }
                 </div>
                 <div className="bg-white p-4 rounded">Right</div>
             </div>
