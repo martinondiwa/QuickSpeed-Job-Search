26afeaf52ddf57791939c343610588e34962ec6a
martinondiwa <surekondiwa@gmail.com>
2024-10-13 23:41:10 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
       //function for the next page
       const nextPage = () => {
        if (currentPage < Math.ceil(filterItems.length / itemsParPage)){
-        setCurrentPage(currentPage)
+        setCurrentPage(currentPage + 1)
        }
       }
       
