8271ed8ca1191276fe5466080bd271ac1aebbd27
martinondiwa <surekondiwa@gmail.com>
2024-10-13 22:56:52 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -59,7 +59,9 @@ const Home = () => {
                 <div className="bg-white p-4 rounded"><Sidebar handleChange={handleChange} /></div>
                 <div className="col-span-2 bg-slate-200 p-4 rounded-md">
                   {
-                    isLoading ? (<p>Loading.....</p>) :  result.length > 0 ?  (<Jobs result={result} />)
+                    isLoading ? (<p>Loading.....</p>) :  result.length > 0 ?  (<Jobs result={result} />) : <>
+                     <h3>{result.length}</h3>
+                    </>
                   }
                 </div>
                 <div className="bg-white p-4 rounded">Right</div>
