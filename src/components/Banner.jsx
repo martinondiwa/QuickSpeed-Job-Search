198ed80d168505af1bd43a1814e73d8318d69183
martinondiwa <surekondiwa@gmail.com>
2024-10-13 17:22:32 +0300

    aad

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -23,13 +23,13 @@ const Banner = ({query, handleInputChange}) => {
            <IoSearchCircleSharp className="absolute mt-2.5 ml-2 text-gray-400" />
           </div>
 
-          <div className="flex md:rounded-s-md rounded shadow-sm ring-2 ring-inset focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3 w-full">
+          <div className="flex md:rounded-s-none rounded shadow-sm ring-2 ring-inset focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3 w-full">
            <input type="text" name="title" id="title" placeholder="Location"  className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholdercolor: text-gray-400 focus:right-0 sm:leading-6 "
            />
            <LuMapPin className="absolute mt-2.5 ml-2 text-gray-400" />
           </div>
 
-          <button type="submit" className="bg-blue py-2 px-8 text-white md:rounded-md rounded"></button>
+          <button type="submit" className="bg-blue py-2 px-8 text-white md:rounded-md rounded">Search</button>
         </div>
       </form>
     </div>
