029d4985fb8eb3dc51c895bc24ae53ce3ec346cc
martinondiwa <surekondiwa@gmail.com>
2024-10-13 16:54:00 +0300

    a

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Banner from "../components/Banner"
 
 const Home = () => {
+
+  const [query, setQuery] = useState ("");
+  const handleInputChange = (event) => {
+      setQuery(event.target.value)
+
+  }
   return (
     <div className="text-blue">
       <Banner/>
