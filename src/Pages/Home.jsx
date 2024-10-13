fa5cdd1df758514a05745802b5e8e95092d9ba93
martinondiwa <surekondiwa@gmail.com>
2024-10-13 22:44:19 +0300

    asd

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
             .then((res) => res.json())
             .then((data) => {
                 setJobs(data);
+                setIsLoading(false);
             });
     }, []);
 
