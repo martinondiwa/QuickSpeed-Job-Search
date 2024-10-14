f6cd6e5f4662b3f7378f91be3c0e211bbd8e8953
martinondiwa <surekondiwa@gmail.com>
2024-10-14 21:35:21 +0300

    ad

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import Location from './Location';
-import Salary from './Salary';
-import JobPostingData from './JobPostingData';
+import React from 'react'
+import Location from './Location'
+import Salary from './Salary'
+import JobPostingData from './JobPostingData'
 
-const Sidebar = ({ handleChange, handleClick }) => {
+const Sidebar = ({handleChange, handleClick}) => {
   return (
     <div className='space-y-5'>
-      <h3 className='text-lg font-bold mb-2'>Filters</h3>
-      {/* Location Filter */}
-      <Location handleChange={handleChange} />
-      {/* Salary Filter */}
-      <Salary handleChange={handleChange} handleClick={handleClick} />
-      {/* Job Posting Date Filter */}
-      <JobPostingData handleChange={handleChange} />
-    </div>
-  );
-};
+    <h3 className='text-lg font-bold mb-2'>Filters</h3>
 
-export default Sidebar;
+    <Location handleChange={handleChange}/>
+    <Salary handleChange={handleChange} handleClick={handleClick}/>
+    <JobPostingData handleChange={handleChange}/>
+    </div>
+  )
+}
+
+export default Sidebar
