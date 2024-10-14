aef608e29815e43a76bcf15edb53144cdaa2aaba
martinondiwa <surekondiwa@gmail.com>
2024-10-14 19:07:01 +0300

    asd

--- src/sidebar/Salary.jsx
+++ src/sidebar/Salary.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Salary = (handleChange, handleClick) => {
   return (
     <div>
-      
+      <h4 className='text-lg font-medium mb-2'>Salary</h4>
+      <div></div>
     </div>
   );
 }
