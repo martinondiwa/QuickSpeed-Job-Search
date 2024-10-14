e400821eac80a2b74a56ef7569d5b4b330b14047
martinondiwa <surekondiwa@gmail.com>
2024-10-14 19:33:53 +0300

    as

--- src/sidebar/Button.jsx
+++ src/sidebar/Button.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Button = (onClickHandler, value, title ) => {
+const Button = ({ onClickHandler, value, title }) => {
   return (
- <button onClick={onClickHandler} value={value} className={'px-4 py-1 border text-base hover:bg-blue'}>
-  {title}
- </button>
+    <button 
+      onClick={onClickHandler} 
+      value={value} 
+      className={'px-4 py-1 border text-base hover:bg-blue'}
+    >
+      {title}
+    </button>
   );
 }
 
--- src/sidebar/Salary.jsx
+++ src/sidebar/Salary.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
-const Salary = (handleChange, handleClick) => {
+const Salary = ({ handleChange, handleClick }) => {
   return (
     <div>
-      <h4 className='text-lg font-medium mb-2'>Salary</h4>
+      <h4 className="text-lg font-medium mb-2">Salary</h4>
+      <div className="mb-4">
+        <Button onClickHandler={handleClick} value="Hourly" title="Hourly" />
+        <Button onClickHandler={handleClick} value="Monthly" title="Monthly" />
+        <Button onClickHandler={handleClick} value="Yearly" title="Yearly" />
+      </div>
       <div>
-      <Button onClickHandler={handleClick} value="Hourly" title="Hourly"/>
-      <Button onClickHandler={handleClick} value="Monthly" title="Monthly"/>
-      <Button onClickHandler={handleClick} value="Mearly" title="Yearly"/>
+        <label className="sidebar-label-container">
+          <input
+            type="radio"
+            name="location"
+            value=""
+            onChange={handleChange}
+          />
+          <span className="checkmark"></span> All
+        </label>
       </div>
     </div>
   );
-}
+};
 
 export default Salary;
