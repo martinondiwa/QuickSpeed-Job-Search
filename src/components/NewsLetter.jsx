e59688daf6818418c2f26341e27f7e28ab75720f
martinondiwa <surekondiwa@gmail.com>
2024-10-15 08:37:13 +0300

    asd

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -11,7 +11,7 @@ const NewsLetter = () => {
         <div className='w-full space-y-4'>
          <input type="email" name='email' id='email' placeholder='name@gmail.com'
          className='w-full block py-2 p1-3 border focus:outline-none'/>
-         <input type="submit" value={"Subscribe"} className='w-full block py-2 p1-3 border focus:outline-none'/>
+         <input type="submit" value={"Subscribe"} className='w-full block py-2 p1-3 border focus:outline-none bg-blue rounded-full text-yellow-50'/>
 
         </div>
       </div>
