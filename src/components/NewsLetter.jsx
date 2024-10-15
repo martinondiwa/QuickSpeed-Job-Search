4e759ab6aaa29a2d85b0923f6dd442b9f11f5595
martinondiwa <surekondiwa@gmail.com>
2024-10-15 08:27:04 +0300

    ad

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -6,7 +6,7 @@ const NewsLetter = () => {
     <div>
       <div>
          <h3  className='text-lg font-bold mb-2 flex items-center gap-2'> <FaEnvelopeOpenText/> Email me for Jobs </h3>
-         <p className=''>Stay updated with the latest job openings and career tips straight to your inbox. Subscribe now, and never miss out on exciting opportunities tailored just for you. Email us today to get started on your job hunt!</p>
+         <p className='text-primary/70 text-base mb-4'>Stay updated with the latest job openings and career tips straight to your inbox. Subscribe now, and never miss out on exciting opportunities tailored just for you. Email us today to get started on your job hunt!</p>
 
 
       </div>
