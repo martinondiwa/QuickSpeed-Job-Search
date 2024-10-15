c62e8c65e6fea2d7e0ac0fe04c659d5a7daacd29
martinondiwa <surekondiwa@gmail.com>
2024-10-15 08:22:19 +0300

    asd

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaEnvelopeOpenText } from 'react-icons/fa'
 
 const NewsLetter = () => {
   return (
     <div>
       <div>
-         <h3>Email me for Jobs </h3>
+         <h3  className='text-lg font-bold mb-2 flex items-center gap-2'> <FaEnvelopeOpenText/> Email me for Jobs </h3>
       </div>
     </div>
   )
