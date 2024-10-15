47cd340ea81fb218a4f0f07dc6f8150360295091
martinondiwa <surekondiwa@gmail.com>
2024-10-15 07:34:23 +0300

    AS

--- src/sidebar/Workexperience.jsx
+++ src/sidebar/Workexperience.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import InputField from '../components/InputField';
 
 const Workexperience = ({handleChange}) => {
   return (
     <div>
-      
+    <h4 className='text-lg font-medium mb-2'>Location</h4>
+    <div>
+        <label className='sidebar-label-container'>
+            <input
+                type="radio"
+                name='location'
+                value=""
+                onChange={handleChange}
+            />
+            <span className='checkmark'></span> All
+        </label>
+
+        <InputField handleChange={handleChange} value="london" title="London" name="location" />
+        <InputField handleChange={handleChange} value="seattle" title="Seattle" name="location" />
+        <InputField handleChange={handleChange} value="brussels" title="Brussels" name="location" />
+        <InputField handleChange={handleChange} value="san Francisco" title="San Francisco" name="location" />
+        <InputField handleChange={handleChange} value="boston" title="Boston" name="location" />
+        <InputField handleChange={handleChange} value="madrid" title="Madrid" name="location" />
     </div>
+</div>
   );
 }
 
