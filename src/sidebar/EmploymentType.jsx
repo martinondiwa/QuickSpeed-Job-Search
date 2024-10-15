33c692b2fc41ce837dd9561ba19f76ffdb959313
martinondiwa <surekondiwa@gmail.com>
2024-10-15 08:05:16 +0300

    ad

--- src/sidebar/EmploymentType.jsx
+++ src/sidebar/EmploymentType.jsx
@@ -4,21 +4,24 @@ import InputField from '../components/InputField';
 const EmploymentType = ({handleChange}) => {
   return (
     <div>
-    <label className='sidebar-label-container'>
-        <input
-            type="radio"
-            name='location'
-            value=""
-            onChange={handleChange}
-        />
-        <span className='checkmark'></span> Any Experience
-    </label>
+    <h4 className='text-lg font-medium mb-2'>Work Experience</h4>
+    <div>
+        <label className='sidebar-label-container'>
+            <input
+                type="radio"
+                name='location'
+                value=""
+                onChange={handleChange}
+            />
+            <span className='checkmark'></span> Any Experience
+        </label>
 
-    <InputField handleChange={handleChange} 
-    value="Internship" title="Internship" name="test" />
-    <InputField handleChange={handleChange} 
-    value="work Remotely" title="Work Remotely" name="test" />
-    
+        <InputField handleChange={handleChange} 
+        value="Internship" title="Internship" name="test" />
+        <InputField handleChange={handleChange} 
+        value="work Remotely" title="Work Remotely" name="test" />
+        
+    </div>
 </div>
   );
 }
