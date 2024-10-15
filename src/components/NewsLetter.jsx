963bb505f9e52df3dd23c2ac74fcc949181adf09
martinondiwa <surekondiwa@gmail.com>
2024-10-15 08:32:22 +0300

    asd

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -9,7 +9,8 @@ const NewsLetter = () => {
          <p className='text-primary/70 text-base mb-4'>Stay updated with the latest job openings and career tips straight to your inbox. Subscribe now, and never miss out on exciting opportunities tailored just for you. Email us today to get started on your job hunt!</p>
         
         <div className='w-full space-y-4'>
-         <input type="email" name='email' id='email' placeholder='name@gmail.com'/>
+         <input type="email" name='email' id='email' placeholder='name@gmail.com'
+         className='w-full block py-2 p1-3 border focus:outline-none'/>
         </div>
       </div>
     </div>
